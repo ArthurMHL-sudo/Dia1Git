78a5e96f897127f3582a6d104604754b750f6f97
ArthurMHL-sudo <ruhtramourao@gmail.com>
2026-05-01 22:17:30 +0100

    Meu Segundo code

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b21de8a0038731eb7cce086b06fed883c0be057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756069645e9b9a4b36588962d15641451126f637")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
